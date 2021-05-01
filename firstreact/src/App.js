299efb3422dd27eef0c280a09cbef213dae0afd1
Shahab Ayyub <68328438+Ayyubshahab786@users.noreply.github.com>
2021-05-01 17:31:51 +0500

    bs yr

--- firstreact/src/App.js
+++ firstreact/src/App.js
@@ -1,9 +1,11 @@
+import react from 'react';
 import './App.css';
+
 function App() {
   return (
     <div className="App">
       <div> <h1>hello react js </h1>
-      <p>now we change to check wether automation deploy is working or not</p>
+      <p>now we change to check wether automation deploy is</p>
       </div>
     </div>
   );
